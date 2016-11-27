b5e713b29d5e463adc2370f59989fce35d3ccbfe
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-11-27 13:38:26 +0900

    [fix]環境設定の変更

--- app/helpers/judge_systems_helper.rb
+++ app/helpers/judge_systems_helper.rb
@@ -1,21 +1,2 @@
- require 'csv'   # csv操作を可能にするライブラリ
- require 'kconv'
- module JudgeSystemsHelper
-
-  def import_csv(csv_file)
-  # csvファイルを受け取って文字列にする
-  csv_text = csv_file.read
-
-  data = ""
-
-  #文字列をUTF-8に変換
-  CSV.parse(Kconv.toutf8(csv_text)) do |row|
-   row.each do |obj|
-     data += obj
-     data += "\n"
-   end
- end
- data  
-end
-module_function :import_csv
+module JudgeSystemsHelper
 end
