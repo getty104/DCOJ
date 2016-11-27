01c1ebc19363b2ba2ed600870dc1154dfd5a83aa
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-11-27 16:09:59 +0900

    [fix]remember meを使うとエラーが出るバグを解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	has_many :codes
 	has_secure_password
   validates :account, presence: true, uniqueness: { case_sensitive: false }
-
+  attr_accessor :remember_token
  # 与えられた文字列のハッシュ値を返す
  def User.digest(string)
  	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
