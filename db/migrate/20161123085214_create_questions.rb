0e410da76f7808ee15dae3d6a25b0d400f8fd8cc
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-12-03 00:10:46 +0900

    [fix]現在herokにうまdeployできないので実験

--- db/migrate/20161123085214_create_questions.rb
+++ db/migrate/20161123085214_create_questions.rb
@@ -1,8 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
-
       t.timestamps
+      t.references :created_user, index: true
+      t.foreign_key :questions, :users, column: :created_user_id
     end
   end
+
 end
--- db/migrate/20161202133318_add_created_user_to_questions.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedUserToQuestions < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :questions, :created_user, foreign_key: true
-  end
-end
