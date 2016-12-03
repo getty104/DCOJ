950f9919caf8d3a4e2e1a54868b670f763708f26
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-12-03 16:32:13 +0900

    [fix]dbの修正

--- db/migrate/20161123085214_create_questions.rb
+++ db/migrate/20161123085214_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
       t.timestamps
-      t.integer :created_user_id
+      t.references :created_user
       t.foreign_key :users, column: :created_user_id
     end
   end
--- db/migrate/20161202134408_create_questions_users.rb
+++ db/migrate/20161202134408_create_questions_users.rb
@@ -1,8 +1,8 @@
 class CreateQuestionsUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :questions_users, id: false do |t|
-      t.integer :user_id
-      t.integer :question_id
+      t.references :user
+      t.references :question
       t.foreign_key :users
       t.foreign_key :questions
     end
