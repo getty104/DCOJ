0ab07b6438c5638318953104d0843f39bcb0f80a
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-12-03 16:18:53 +0900

    [fix]dbの修正

--- db/migrate/20161123085214_create_questions.rb
+++ db/migrate/20161123085214_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
       t.timestamps
+      
       t.integer :created_user_id
       t.foreign_key :users, column: :created_user_id
     end
