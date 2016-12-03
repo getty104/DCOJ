3b14b561c7406e11cd3a518993e68f6e9cb30e7e
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-12-03 17:04:07 +0900

    [fix]dbの修正

--- db/migrate/20161202134408_create_questions_users.rb
+++ db/migrate/20161202134408_create_questions_users.rb
@@ -1,8 +1,8 @@
 class CreateQuestionsUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :questions_users, id: false do |t|
-      t.references :user, index: true
-      t.references :question, index: true
+      t.references :user, null: false, index: true
+      t.references :question, null: false, index: true
       t.foreign_key :users
       t.foreign_key :questions
     end
