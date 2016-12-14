1f5f578f9f31ccaef35874e8db0ff91fda4a4d4d
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-12-14 23:01:03 +0900

    [fix]タイムラインのレイアウト変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require bootstrap
 //= require turbolinks
 //= require_tree .
