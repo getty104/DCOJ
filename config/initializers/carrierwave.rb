0cafe0226913a18472cd2b4ef3f696eaf4f8fff7
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-12-29 00:15:36 +0900

    [fix]バグ修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV[ACCESS],
-    :aws_secret_access_key  => ENV[SECRET],
+    :aws_access_key_id      => ENV['ACCESS'],
+    :aws_secret_access_key  => ENV['SECRET'],
     :region                 => 'ap-northeast-1',
     :path_style             => true
   }
