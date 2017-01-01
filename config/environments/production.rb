fa01778c2deba6f58bb20ac6d3328abb2650873c
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-01-01 22:37:02 +0900

    [fix]ドメイン設定の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,19 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+# deviseの設定
+config.action_mailer.default_url_options = { host: 'dcoj.herokuapp.com' }
+# mail setting
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :user_name => "hayabusatoshihumi@gmail.com",
+  :password => "yvtfwbpfgptbqfpx",
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
