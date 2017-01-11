203ca6d155ea2278e11aca03f9f0e30d8e070b34
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-01-11 17:52:29 +0900

    [fix]レイアウトの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ config.action_mailer.smtp_settings = {
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.static_cache_control = "public, max-age=31536000"
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
   config.assets.compress = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
