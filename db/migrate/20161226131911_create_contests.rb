b33c489b2f8c3cd9536061304a7298dae9e416ff
清野隼史 <kiyonotoshifumi@kiyonoshunshi-no-MacBook-Air-2.local>
2017-02-18 15:08:09 +0900

    [fix]データベースの修正

--- db/migrate/20161226131911_create_contests.rb
+++ db/migrate/20161226131911_create_contests.rb
@@ -4,9 +4,8 @@ class CreateContests < ActiveRecord::Migration[5.0]
 			t.time :start_time
 			t.time :finish_time
 			t.references :created_user, null: false, index: true
-			t.foreign_key :users, :column => "created_user_id"
-
 			t.timestamps
 		end
+			add_foreign_key :contests, :users, :column => "created_user_id"
 	end
 end
