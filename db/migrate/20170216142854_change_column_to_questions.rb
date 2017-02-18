877dc9bd196d9f6cc1443c09570724dd1cafb672
清野隼史 <kiyonotoshifumi@kiyonoshunshi-no-MacBook-Air-2.local>
2017-02-18 15:18:01 +0900

    [fix]データベースの修正

--- db/migrate/20170216142854_change_column_to_questions.rb
+++ db/migrate/20170216142854_change_column_to_questions.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToQuestions < ActiveRecord::Migration[5.0]
   def change
-  	 change_column :questions, :for_contest, :integer, null: false
+  	 change_column :questions, :for_contest, 'integer USING CAST(for_contest AS integer)'
   end
 end
