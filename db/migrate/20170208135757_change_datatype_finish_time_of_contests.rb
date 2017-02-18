6df30772c013b5e023caf5ba5eecd932217698be
清野隼史 <kiyonotoshifumi@kiyonoshunshi-no-MacBook-Air-2.local>
2017-02-18 14:24:20 +0900

    [fix]データベースの修正

--- db/migrate/20170208135757_change_datatype_finish_time_of_contests.rb
+++ db/migrate/20170208135757_change_datatype_finish_time_of_contests.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeFinishTimeOfContests < ActiveRecord::Migration[5.0]
   def change
-  	  	change_column :contests, :finish_time, :datetime
+  	  	change_column :contests, :finish_time, 'datetime USING CAST(finish_time AS datetime)'
   end
 end
--- db/migrate/20170211054619_create_joins.rb
+++ db/migrate/20170211054619_create_joins.rb
@@ -1,9 +1,11 @@
 class CreateJoins < ActiveRecord::Migration[5.0]
 	def change
 		create_table :joins do |t|
-			t.belongs_to :contest
-			t.belongs_to :user
+			t.belongs_to :contest, index: true
+			t.belongs_to :user, index: true
 			t.timestamps
 		end
+		add_foreign_key :joins, :contests
+		add_foreign_key :joins, :users  
 	end
 end
