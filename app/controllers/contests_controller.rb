884534d45e2b88ae6381111f61f32d7e52f0171b
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-03-09 14:45:56 +0900

    [fix]

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -165,6 +165,10 @@ class ContestsController < ApplicationController
 			end
 		end
 
+		def arc_gauss(x)
+			return (-0.5*(Math.log(1-x**2)))**(Math.PI/2)
+		end
+
 
 
 	end
--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -7,7 +7,4 @@ module ContestsHelper
 		return "#{hour}:#{minute}:#{sec}"
 	end
 
-	def arc_gauss(x)
-		return (-0.5*(Math.log(1-x**2)))**(Math.PI/2)
-	end
 end
