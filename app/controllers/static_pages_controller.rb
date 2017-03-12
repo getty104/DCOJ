6123caf81f5969524ff1e940bc7ffb28d37fcdc9
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-03-12 11:59:30 +0900

    [update]名前にレーティングの色を適用

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,8 +8,8 @@ class StaticPagesController < ApplicationController
 	def main_menu
 		authenticate_user!
 		@posts = Post.order("created_at DESC").includes(:user).includes(:question).includes(:contest).page(params[:page]).per(10)
-		@created_users = User.all.select(:name, :created_question_number).limit(5).order('created_question_number DESC')
-		@solved_users = User.all.select(:name, :solved_question_number).limit(5).order('solved_question_number DESC')
+		@created_users = User.all.select(:name, :created_question_number, :rate).limit(5).order('created_question_number DESC')
+		@solved_users = User.all.select(:name, :solved_question_number, :rate).limit(5).order('solved_question_number DESC')
 		@rate_users = User.all.select(:name, :rate).limit(5).order('rate DESC')
 		@recent_contests = current_user.contests.future_contests.select(:id,:title, :start_time).order('start_time DESC')
 		@now_contests = current_user.contests.now_contests.select(:id, :title, :start_time).limit(10).order('start_time DESC')
