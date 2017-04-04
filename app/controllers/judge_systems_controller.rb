b50b73f2bd448dd26a206fa2396e7cf1094efcbb
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-04-05 00:05:47 +0900

    [fix]バグ守勢

--- app/controllers/judge_systems_controller.rb
+++ app/controllers/judge_systems_controller.rb
@@ -52,15 +52,15 @@ class JudgeSystemsController < ApplicationController
 					update_solved_data
 					case @question.question_level.to_i
 					when 1
-						@join.update_columns( score: @join.score + 100 * 1, amount_time: @join.amount_time + (Time.now - @contest.start_time).to_i, level1_solve_time: (Time.now - @contest.start_time).to_i )
+						@join.update_status 1
 					when 2
-						@join.update_columns( score: @join.score + 100 * 2, amount_time: @join.amount_time + (Time.now - @contest.start_time).to_i, level2_solve_time: (Time.now - @contest.start_time).to_i )
+						@join.update_status 2
 					when 3
-						@join.update_columns( score: @join.score + 100 * 3, amount_time: @join.amount_time + (Time.now - @contest.start_time).to_i, level3_solve_time: (Time.now - @contest.start_time).to_i )
+						@join.update_status 3
 					when 4
-						@join.update_columns( score: @join.score + 100 * 4, amount_time: @join.amount_time + (Time.now - @contest.start_time).to_i, level4_solve_time: (Time.now - @contest.start_time).to_i )
+						@join.update_status 4
 					when 5
-						@join.update_colmuns( score: @join.score + 100 * 5, amount_time: @join.amount_time + (Time.now - @contest.start_time).to_i, level5_solve_time: (Time.now - @contest.start_time).to_i )
+						@join.update_status 5
 					end
 					RankSystem.update_ranking(@contest)
 				end
@@ -109,23 +109,6 @@ class JudgeSystemsController < ApplicationController
 
 		def first_time?
 			current_user != @question.created_user && current_user.questions.include?(@question) == false
-		end
-
-		def update_status question_level
-			@join = Join.find_by( contest_id: @contest.id, user_id: current_user.id )
-			case question_level
-			when 1
-				@join.update_attributes( score: score + 100 * question_level, amount_time: amount_time + (Time.now - contest.start_time).to_i, level1_solve_time: (Time.now - contest.start_time).to_i )
-			when 2
-				@join.update_attributes( score: score + 100 * question_level, amount_time: amount_time + (Time.now - contest.start_time).to_i, level2_solve_time: (Time.now - contest.start_time).to_i )
-			when 3
-				@join.update_attributes( score: score + 100 * question_level, amount_time: amount_time + (Time.now - contest.start_time).to_i, level3_solve_time: (Time.now - contest.start_time).to_i )
-			when 4
-				@join.update_attributes( score: score + 100 * question_level, amount_time: amount_time + (Time.now - contest.start_time).to_i, level4_solve_time: (Time.now - contest.start_time).to_i )
-			when 5
-				@join.update_attributes( score: score + 100 * question_level, amount_time: amount_time + (Time.now - contest.start_time).to_i, level5_solve_time: (Time.now - contest.start_time).to_i )
-			end
-
 		end
 
 	end
